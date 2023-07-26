0a027595deb59ade29708403fc0fac469cd6cfbf
Elena Abueid <139688882+elenaAb24-meet@users.noreply.github.com>
2023-07-26 17:01:25 +0300

    jj

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -2,11 +2,56 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config={
+  "apiKey": "AIzaSyCKYIjf92qJr8xux2xGyb58JunfNMzZXy0",
+  "authDomain": "elena-94419.firebaseapp.com",
+  "projectId": "elena-94419",
+  "storageBucket": "elena-94419.appspot.com",
+  "messagingSenderId": "554917943404",
+  "appId": "1:554917943404:web:24a1b0cbab82ee79ada049",
+  'measurementId': "G-QNFK0V3H22",
+  "databaseURL": "https://elena-94419-default-rtdb.firebaseio.com"
+  }
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database ()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-#Code goes below here
 
+users = []
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('survey'))
+        except:
+            error = "Authentication failed"
+    return render_template('login.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        username = request.form['username']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('survey'))
+        except:
+            error = "Authentication failed"
+    return render_template('signup.html')
+
+@app.route('/survey')
+def survey():
+    return render_template('survey.html')
 
 
 
